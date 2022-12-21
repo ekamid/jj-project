47e69911cd655a12e9ed30eb356b3b0c951d3b11
ekamid <ebrahimkhalilamidbd@gmail.com>
2022-12-21 12:09:44 +0600

    customer register with validation completed

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,12 +36,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'phone' => ['required', 'regex:/^(?:\+88|88)?(01[3-9]\d{8})$/', 'unique:users'],
+            'password' => ['required', 'confirmed', 'min:8', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'phone' => $request->phone,
             'password' => Hash::make($request->password),
         ]);
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,22 +10,53 @@
                     @csrf
                     <div class="key">
                         <i class="fa fa-user" aria-hidden="true"></i>
-                        <input type="text" name="name" required="" placeholder="Full Name">
+                        <input type="text" name="name" required placeholder="Full Name" value="{{ old('name') }}">
                         <div class="clearfix"></div>
                     </div>
+                    @error('name')
+                        <div class="invalid-feedback text-danger" role="alert">
+                            <b>{{ $message }}</b>
+                        </div>
+                    @enderror
+
                     <div class="key">
                         <i class="fa fa-envelope" aria-hidden="true"></i>
-                        <input type="text" name="email" required="" placeholder="Email">
+                        <input type="email" name="email" required placeholder="Email" value="{{ old('email') }}">
                         <div class="clearfix"></div>
                     </div>
+                    @error('email')
+                        <div class="invalid-feedback text-danger" role="alert">
+                            <b>{{ $message }}</b>
+                        </div>
+                    @enderror
+
                     <div class="key">
-                        <i class="fa fa-lock" aria-hidden="true"></i>
-                        <input type="password" name="password" required="" placeholder="Password">
+                        <i class="fa fa-phone" aria-hidden="true"></i>
+                        <input type="text" name="phone" required placeholder="Phone" value="{{ old('phone') }}">
                         <div class="clearfix"></div>
                     </div>
+
+                    @error('phone')
+                        <div class="invalid-feedback text-danger" role="alert">
+                            <b>{{ $message }}</b>
+                        </div>
+                    @enderror
+
                     <div class="key">
                         <i class="fa fa-lock" aria-hidden="true"></i>
-                        <input type="password" name="password_confirmation" required="" placeholder="Confirm Password">
+                        <input type="password" name="password" required placeholder="Password">
+                        <div class="clearfix"></div>
+                    </div>
+
+                    @error('password')
+                        <div class="invalid-feedback text-danger" role="alert">
+                            <b>{{ $message }}</b>
+                        </div>
+                    @enderror
+
+                    <div class="key">
+                        <i class="fa fa-lock" aria-hidden="true"></i>
+                        <input type="password" name="password_confirmation" required placeholder="Confirm Password">
                         <div class="clearfix"></div>
                     </div>
                     <input type="submit" value="Register">
